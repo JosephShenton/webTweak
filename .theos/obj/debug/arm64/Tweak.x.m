97825fa98fa31a23df8e0928d344a9501931a235
Joseph Shenton <joe@jjs.digital>
2020-06-20 14:03:56 +1000

    S

--- .theos/obj/debug/arm64/Tweak.x.m
+++ .theos/obj/debug/arm64/Tweak.x.m
@@ -1,21 +1,47 @@
+#line 1 "Tweak.x"
 #include <spawn.h>
 extern char **environ;
 #define RAND_FROM_TO(min, max) (min + arc4random_uniform(max - min + 1))
 
-%hook UIApplication
-	
-	-(BOOL)openURL:(id)arg1 {
 
-		if ([arg1 isKindOfClass:[NSString class]] || [arg1 isKindOfClass:[NSURL class]]) { // Argument 1 is a String or URL, this is what we want.
+#include <substrate.h>
+#if defined(__clang__)
+#if __has_feature(objc_arc)
+#define _LOGOS_SELF_TYPE_NORMAL __unsafe_unretained
+#define _LOGOS_SELF_TYPE_INIT __attribute__((ns_consumed))
+#define _LOGOS_SELF_CONST const
+#define _LOGOS_RETURN_RETAINED __attribute__((ns_returns_retained))
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+
+@class UIApplication; 
+static BOOL (*_logos_orig$_ungrouped$UIApplication$openURL$)(_LOGOS_SELF_TYPE_NORMAL UIApplication* _LOGOS_SELF_CONST, SEL, id); static BOOL _logos_method$_ungrouped$UIApplication$openURL$(_LOGOS_SELF_TYPE_NORMAL UIApplication* _LOGOS_SELF_CONST, SEL, id); 
+
+#line 5 "Tweak.x"
+
+	
+	static BOOL _logos_method$_ungrouped$UIApplication$openURL$(_LOGOS_SELF_TYPE_NORMAL UIApplication* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1) {
+
+		if ([arg1 isKindOfClass:[NSString class]] || [arg1 isKindOfClass:[NSURL class]]) { 
 			
-			if ([arg1 isKindOfClass:[NSString class]]) { // Argument 1 is a String, treat as such.
+			if ([arg1 isKindOfClass:[NSString class]]) { 
 
 				NSString *url = arg1;
 
-				if([url hasPrefix:@"webtweak://"]) { // Bingo, this means we are on the right track.
+				if([url hasPrefix:@"webtweak://"]) { 
 				    url = [url stringByReplacingOccurrencesOfString:@"webtweak://" withString:@""];
 
-				    // Download
+				    
 				    NSInteger intName = RAND_FROM_TO(1000000000, 1548720128);
 				    NSString *fileName = [@"" stringByAppendingFormat:@"%ld.deb", (long)intName];
 					NSString *filePath = @"/var/mobile/Downloads/";
@@ -32,7 +58,7 @@ extern char **environ;
 					    }
 					}];
 
-				    // Install
+				    
 				    const char *filePathChar = [filePath UTF8String];
 
 				    pid_t pid;
@@ -46,7 +72,7 @@ extern char **environ;
 					posix_spawn(&pid, argv[0], NULL, NULL, argv, environ);
 					waitpid(pid, NULL, 0);
 
-					// Respring
+					
 					pid_t pid2;
 					char *argv2[] = {
 					    "killall",
@@ -60,17 +86,17 @@ extern char **environ;
 
 					return YES;
 				} else {
-					return %orig;
+					return _logos_orig$_ungrouped$UIApplication$openURL$(self, _cmd, arg1);
 				}
 
-			} else if ([arg1 isKindOfClass:[NSURL class]]) { // Argument 1 is a String, treat as such.
+			} else if ([arg1 isKindOfClass:[NSURL class]]) { 
 
 				NSString *url = (NSString*)arg1;
 
-				if([url hasPrefix:@"webtweak://"]) { // Bingo, this means we are on the right track.
+				if([url hasPrefix:@"webtweak://"]) { 
 				    url = [url stringByReplacingOccurrencesOfString:@"webtweak://" withString:@""];
 
-				    // Download
+				    
 				    NSInteger intName = RAND_FROM_TO(1000000000, 1548720128);
 				    NSString *fileName = [@"" stringByAppendingFormat:@"%ld.deb ", (long)intName];
 					NSString *filePath = @"/var/mobile/Downloads/";
@@ -87,7 +113,7 @@ extern char **environ;
 					    }
 					}];
 
-				    // Install
+				    
 
 				    const char *filePathChar = [filePath UTF8String];
 
@@ -102,7 +128,7 @@ extern char **environ;
 					posix_spawn(&pid, argv[0], NULL, NULL, argv, environ);
 					waitpid(pid, NULL, 0);
 
-					// Respring
+					
 					pid_t pid2;
 					char *argv2[] = {
 					    "killall",
@@ -116,13 +142,16 @@ extern char **environ;
 
 					return YES;
 				} else {
-					return %orig;
+					return _logos_orig$_ungrouped$UIApplication$openURL$(self, _cmd, arg1);
 				}
 
 			}
 
 		}
-		return %orig;
+		return _logos_orig$_ungrouped$UIApplication$openURL$(self, _cmd, arg1);
 	}
 
-%end
+
+static __attribute__((constructor)) void _logosLocalInit() {
+{Class _logos_class$_ungrouped$UIApplication = objc_getClass("UIApplication"); MSHookMessageEx(_logos_class$_ungrouped$UIApplication, @selector(openURL:), (IMP)&_logos_method$_ungrouped$UIApplication$openURL$, (IMP*)&_logos_orig$_ungrouped$UIApplication$openURL$);} }
+#line 129 "Tweak.x"
